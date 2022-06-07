608918565519a062f1f3fdd8e3cb8d90de4b22c2
tsukanov1974 <tsukanov1974@yandex.ru>
2022-06-07 23:43:26 +0300

    Добавление файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# A/B Калькулятор
+
+import tkinter as tk
+
+# Создание главного окна
+root = tk.Tk()
+root.geometry("280x300")
+root.title("A/B Калькулятор")
+
+# Запуск цикла mainloop
+root.mainloop()
